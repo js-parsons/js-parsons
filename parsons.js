768457e6a19b7401a4db7a0b3bef3bfbc2f0fafb
Petri Ihantola <petri@tko-add-231.cs.hut.fi>
2010-02-23 12:46:50 +0200

    renaming some functions

--- parsons.js
+++ parsons.js
@@ -1,34 +1,34 @@
 var parsons2d = function(options) {
 	this.options = options;
         //
-	var indents = [];
+	var modified_lines = [];
 	var X_INDENT = options.xIndent || 50;
 	function updateIndent(leftDiff, id) {
-            var code_line = getElem(id);
+            var code_line = getLineById(id);
             var new_indent = code_line.indent
                 + Math.floor(leftDiff / X_INDENT);
             code_line.indent = new_indent;
             return new_indent;
 	};
-        function getElem(id) {
-            return indents[getElemIndex(id)];
+        function getLineById(id) {
+            return modified_lines[getElemIndex(id)];
         }
 	function getElemIndex(id) {
-		for ( var i = 0; i < indents.length; i++) {
-			if (indents[i].id == id) {
+		for ( var i = 0; i < modified_lines.length; i++) {
+			if (modified_lines[i].id == id) {
 				return i;
 			}
 		}
 	};
         function getFeedback() {
             var usersCode = $("#" + this.options.sortableId).sortable('toArray');
-            for ( var i = 0; i < indents.length; i++) {
-                var code_line = getElem(usersCode[i]);
-                if (indents[i].code !== code_line.code) {
+            for ( var i = 0; i < modified_lines.length; i++) {
+                var code_line = getLineById(usersCode[i]);
+                if (modified_lines[i].code !== code_line.code) {
                     alert("line " + (i+1) + " is not correct!");
                     return;
                 }
-                if (indents[i].indent !== this.options.codeLines[i][0]) {
+                if (modified_lines[i].indent !== this.options.codeLines[i][0]) {
                     alert("line " + (i+1) + " is not indented correctly");
                     return;
                 }                
@@ -40,7 +40,7 @@ var parsons2d = function(options) {
 		if (typeof(options.codeLines) === "string") {
 			codelines = options.codeLines.split('\n');
 			for (var i = 0; i < codelines.length; i++) {
-				indents[i] = {
+				modified_lines[i] = {
 						'indent' : 0,
 						'code' : codelines[i],
 						'id' : 'codeline' + i
@@ -49,7 +49,7 @@ var parsons2d = function(options) {
 			}
 		} else {
 			for (var i = 0; i < options.codeLines.length; i++) {
-				indents[i] = {
+				modified_lines[i] = {
 						'indent' : 0,
 						'code' : options.codeLines[i][1],
 						'id' : 'codeline' + i
