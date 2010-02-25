7097e04a6c8141b9c948a3e909e3be0f5e52b07b
Ville Karavirta <vkaravir@tb214.cs.hut.fi>
2010-02-25 17:02:38 +0200

    New instance functionalitty fixed. minor cleanup.

--- parsons.js
+++ parsons.js
@@ -28,17 +28,14 @@ var parsons2d = function(options) {
             'incorrectPosition' : 'incorrectPosition',
             'correctIndent' : 'correctIndent',
             'incorrectIndent' : 'incorrectIndent'};
-    function updateIndent(leftDiff, id) {
+    var updateIndent = function(leftDiff, id) {
         var code_line = getLineById(id);
         var new_indent = code_line.indent + Math.floor(leftDiff / X_INDENT);
         new_indent = Math.max(0, new_indent);
         code_line.indent = new_indent;
         return new_indent;
     };
-    function getLineById(id) {
-        return modified_lines[getElemIndex(id)];
-    }
-    function getElemIndex(id) {
+    var getElemIndex = function(id) {
         for ( var i = 0; i < modified_lines.length; i++) {
             if (modified_lines[i].id == id) {
                 return i;
@@ -46,38 +43,41 @@ var parsons2d = function(options) {
         }
     };
 
-    function normalizeIndents(lines) {
+    var getLineById = function(id) {
+        return modified_lines[getElemIndex(id)];
+    };
+
+    var normalizeIndents = function(lines) {
 
         var normalized = [];
-        var previous_indentation;
         var new_line;
-
+        var match_indent = function(index) {
+            //return line index from the previous lines with matchind indentation
+            for (var i = index-1; i >= 0; i--) {
+                if (lines[i].indent == lines[index].indent) {
+                    return i;
+                }
+            }
+            return -1;
+        };
         for ( var i = 0; i < lines.length; i++ ) {
             //create shallow copy from the line object
             new_line = jQuery.extend({}, lines[i]);
-            if (i == 0) {
+            if (i === 0) {
                 new_line.indent = 0;
             } else if (lines[i].indent == lines[i-1].indent) {
                 new_line.indent = normalized[i-1].indent;
             } else if (lines[i].indent > lines[i-1].indent) {
                 new_line.indent = normalized[i-1].indent + 1;
             } else {
-                // indentation van be -1 if no matching indentation exists, i.e. IndentationError in Python
-                new_line.indent = function(index) {
-                    //return line index from the previous lines with matchind indentation
-                    for (var i = index-1; i >= 0; i--) {
-                        if (lines[i].indent == lines[index].indent) {
-                            return i;
-                        }
-                    }
-                    return -1;
-                }(i);                    
+                // indentation can be -1 if no matching indentation exists, i.e. IndentationError in Python
+                new_line.indent = match_indent(i);                    
             }
             normalized[i] = new_line;
         }
         return normalized;
     };
-    function getModifiedCode() {
+    var getModifiedCode = function() {
         //ids of the the modified code
         var users_code_ids = $("#ul-" + options.sortableId).sortable('toArray');
         var lines_to_return = [];
@@ -87,41 +87,41 @@ var parsons2d = function(options) {
         return lines_to_return;
     };
     
-    function displayError(message) {
-    	if (options.incorrectSound && $.sound) {
+    var displayError = function(message) {
+        if (options.incorrectSound && $.sound) {
             $.sound.play(options.incorrectSound);
         }
-    	alert(message);
+        alert(message);
     };
     
-    function getFeedback() {
+    var getFeedback = function() {
         feedback_exists = true;
         var student_code = normalizeIndents(getModifiedCode());
-        var lines_to_check = Math.min(student_code.length, model_solution.length)
+        var lines_to_check = Math.min(student_code.length, model_solution.length);
         
         for (var i = 0; i < lines_to_check; i++) {
             var code_line = student_code[i];
-            var model_line = model_solution[i]
+            var model_line = model_solution[i];
             if (code_line.code !== model_line.code) {
                 $("#" + code_line.id).addClass("incorrectPosition");
-            	displayError("line " + (i+1) + " is not correct!");
+                displayError("line " + (i+1) + " is not correct!");
                 return;
             }
             if (code_line.indent !== model_line.indent) {
                 $("#" + code_line.id).addClass("incorrectIndent");
-            	displayError("line " + (i+1) + " is not indented correctly");
+                displayError("line " + (i+1) + " is not indented correctly");
                 return;
             }
         }
         
         if (model_solution.length < student_code.length) {
             $("#ul-" + options.sortableId).addClass("incorrect");
-        	displayError("Too many lines in your solution");
-        	return;        	
+            displayError("Too many lines in your solution");
+            return;
         } else if (model_solution.length > student_code.length){
             $("#ul-" + options.sortableId).addClass("incorrect");
-        	displayError("Too few lines in your solution");
-        	return;
+            displayError("Too few lines in your solution");
+            return;
         }        
         
         if (options.correctSound && $.sound) {
@@ -131,7 +131,7 @@ var parsons2d = function(options) {
         $("#ul-" + options.sortableId).addClass("correct");
         alert("ok");
     };
-    function clearFeedback() {
+    var clearFeedback = function() {
         if (feedback_exists) {
             $("#ul-" + options.sortableId).removeClass("incorrect correct");
             var li_elements = $("#ul-" + options.sortableId + " li");
@@ -141,16 +141,16 @@ var parsons2d = function(options) {
         }
         feedback_exists = false;
     };
-    function init() {
+    var init = function() {
         var codelines = [];
         for (var i = 0; i < options.codeLines.length; i++) {
             modified_lines[i] = codeLine(options.codeLines[i], 'codeline' + i);
-            modified_lines[i].indent = 0;
             if (modified_lines[i].indent < 0) {
                 extra_lines.push(codeLine(options.codeLines[i]));
             } else {
                 model_solution.push(codeLine(options.codeLines[i]));
             }
+            modified_lines[i].indent = 0;
             codelines[i] = '<li id="codeline' + i + '" class="prettyprint lang-py">' + modified_lines[i].code + '<\/li>';
         }
         var swap1, swap2, tmp;
@@ -170,36 +170,36 @@ var parsons2d = function(options) {
         if (typeof(options.prettyPrint) === "undefined" || options.prettyPrint) {
             prettyPrint();
         }
+        var sortable = $("#ul-" + options.sortableId).sortable({
+            start : clearFeedback,
+            stop : function(event, ui) {
+                if ($(event.target)[0] != ui.item.parent()[0]) {
+                    return;
+                }
+                var ind = updateIndent(ui.position.left - ui.item.parent().offset().left,
+                                        ui.item[0].id);
+                ui.item.css("margin-left", X_INDENT * ind + "px");
+            },
+            receive : function(event, ui) {
+                var ind = updateIndent(ui.position.left - ui.item.parent().offset().left,
+                                        ui.item[0].id);
+                ui.item.css("margin-left", X_INDENT * ind + "px");
+            },
+            grid : [ X_INDENT, 1 ]
+        });
+        if (options.trashId) {
+            var trash = $("#ul-" + options.trashId).sortable({
+                connectWith: sortable,
+                start: clearFeedback,
+                receive: function(event, ui) {
+                    getLineById(ui.item[0].id).indent = 0;
+                    ui.item.css("margin-left", "0");
+                }
+            });
+            sortable.sortable('option', 'connectWith', trash);
+        }
     };
     init();
-    var sortable = $("#ul-" + options.sortableId).sortable({
-        start : clearFeedback,
-        stop : function(event, ui) {
-            if ($(event.target)[0] != ui.item.parent()[0]) {
-                return;
-            }
-            var ind = updateIndent(ui.position.left - ui.item.parent().offset().left,
-                                    ui.item[0].id);
-            ui.item.css("margin-left", X_INDENT * ind + "px");
-        },
-        receive : function(event, ui) {
-            var ind = updateIndent(ui.position.left - ui.item.parent().offset().left,
-                                    ui.item[0].id);
-            ui.item.css("margin-left", X_INDENT * ind + "px");
-        },
-        grid : [ X_INDENT, 1 ]
-    });
-    if (options.trashId) {
-        var trash = $("#ul-" + options.trashId).sortable({
-            connectWith: sortable,
-            start: clearFeedback,
-            receive: function(event, ui) {
-                getLineById(ui.item[0].id).indent = 0;
-                ui.item.css("margin-left", "0");
-            }
-        });
-        sortable.sortable('option', 'connectWith', trash);
-    }
     return {
         getFeedback : function() {
             getFeedback();
